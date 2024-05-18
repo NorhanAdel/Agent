4b33c667964adfd174be7a08b7359a6e5c465cfa
menna727q <mennaer727@gmail.com>
2024-05-18 17:48:41 +0300

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,29 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const activityRoutes = require('./routes/activity');
+const userRoutes= require('./routes/users');
+const permissionRoutes = require('./routes/permission');
+const contactRoutes = require('./routes/client');
+const clientActivities = require('./routes/clientActivities');
 const app = express();
-const uploadRoute = require("./routes/uploadRoute");
+app.use(bodyParser.json());
 
-// Middleware to handle JSON request bodies
-app.use(express.json());
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://menna:menna123456789@projecterp.lccueui.mongodb.net/Agent' )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
 
-// Mount the upload route
-app.use(uploadRoute);
+// Routes
+app.use('/api', activityRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api',permissionRoutes);
+app.use('/api', contactRoutes);
+app.use('/api', clientActivities);
+
+
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
