105f437ede21a8c6db1926a0233f003215ccb017
Nourhan Adel <91224846+NorhanAdel@users.noreply.github.com>
2025-11-04 15:07:58 +0200

    Delete Backend directory

--- Backend/routes/agency.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const uploadController = require("../controllers/uploadController");
-const upload = require("../middlewares/uploadMiddleware"); // Middleware for handling file upload
-
-// Define the upload route
-router.post("/upload", upload.single("file"), uploadController.uploadFile);
-
-module.exports = router;
--- Backend/validation.js
+++
@@ -1,7 +0,0 @@
-const joi = require('joi');
-
-const ActivitySchema = joi.object({
-  Activity_Type: joi.string().required(),
-}).required();
-
-module.exports = { ActivitySchema };
